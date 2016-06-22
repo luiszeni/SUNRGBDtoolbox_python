d6380004e48475aba2e0bfd4ca764172ad5bb226
luis zeni <luisfelipezeni@gmail.com>
2016-06-22 18:38:27 -0300

    Import of 2D annotations(labels and polygon)

--- SUNRGBD.py
+++ SUNRGBD.py
@@ -1,31 +1,50 @@
 import cv2
 from os import listdir
-
+import json
+import numpy as np
 
 class FrameData:
-	def __init__(self, imgRGB,imgD):
+	def __init__(self, imgRGB,imgD,annotation2D,labels2D):
 		self.imgRGB = imgRGB
 		self.imgD = imgD
-
+		self.annotation2D = annotation2D
+		self.labels2D = labels2D
 		
 def readFrame( framePath, bfx ):
-	
+	#read RGB information to numpy array	
 	rgbPath = framePath + "/image/" 
 	rgbPath += listdir(rgbPath)[0]
+	imgRGB = cv2.imread(rgbPath);
 	
+	#read depth information to numpy array
 	if not(bfx):
 		depthPath = framePath + "/depth/" 
 	else:
 		depthPath = framePath + "/depth_bfx/" 
-		
 	depthPath += listdir(depthPath)[0]
-	
-	print(rgbPath)
-	print(depthPath)
-  
-	imgRGB = cv2.imread(rgbPath);
 	imgD = cv2.imread(depthPath);
-
-	frameData = FrameData(imgRGB,imgD)
 	
+	#read 2D annotations to a list o numpy arrays where each index is related with one object polygon and a list where the index links the object polygon to the object label.
+	anotation2D = framePath + "/annotation2Dfinal/index.json"
+	
+	with open(anotation2D) as data_file:    
+		data = json.load(data_file)
+			
+	numberOfAnot = len(data["frames"][0]["polygon"]);
+	
+	anootation2D = [];
+	labels2D = [];
+	for i in range(0,numberOfAnot):
+		x = data["frames"][0]["polygon"][i]["x"]
+		y = data["frames"][0]["polygon"][i]["y"]
+
+		idxObj = data["frames"][0]["polygon"][i]["object"];
+		pts2 = np.array([x,y], np.int32)
+		pts2 = np.transpose(pts2);
+		anootation2D.append(pts2);
+		
+		labels2D.append(data['objects'][idxObj]["name"])
+		
+	frameData = FrameData(imgRGB,imgD,anootation2D,labels2D)
+
 	return frameData;
--- usageExample.py
+++ usageExample.py
@@ -1,12 +1,25 @@
 import cv2
 import SUNRGBD
+import random as rand
+import numpy as np
 
 frameData = SUNRGBD.readFrame( "/home/zeni/Pictures/SUNRGBD/kv2/kinect2data/000003_2014-05-26_14-24-42_260595134347_rgbf000040-resize", True );
 
-cv2.imshow("RGB Image",frameData.imgRGB);
-cv2.imshow("Detph Image",frameData.imgD);
+imgRGBWithAnnotations = np.array(frameData.imgRGB, copy=True);
+
+for i in range(0, len(frameData.annotation2D)):
+
+	color = [rand.randint(0,255), rand.randint(0,255), rand.randint(0,255)]
+		
+	cv2.fillPoly(imgRGBWithAnnotations, [frameData.annotation2D[i]], color)
+
+for i in range(0, len(frameData.annotation2D)):	
+	data = frameData.annotation2D[i];
+	centroid = np.mean(data,axis=0)
+	cv2.putText(imgRGBWithAnnotations, frameData.labels2D[i], (int(centroid[0]), int(centroid[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.7, [0,0,0],2)
+	
+cv2.imshow("Detph Image", frameData.imgD);
+cv2.imshow("RGB Image", frameData.imgRGB);
+cv2.imshow("Annotated Image", imgRGBWithAnnotations);
 
 cv2.waitKey(0);
-	
-
-
